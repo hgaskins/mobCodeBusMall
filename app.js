b100ef9298e6e6d5443ff6dd4f789227b9254441
hannah gaskins <hnag409@gmail.com>
2016-03-29 13:35:22 -0700

    added in clickCount variable and added newImage.setAttribute

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 var catDiv = document.getElementById('catsOnCats');
+
+var newImage = document.getElementById('catImage');
+
 catDiv.addEventListener("click", function() {
   console.log("this is a click");
+  var clickCount = newImage.getAttribute("src");
+  console.log(clickCount);
+  
+  //replacing image
+  var n = randomImageIndex();
+  newImage.setAttribute("src", catArray[n].path);
+  catArray[n].nShow++;
 })
 
 var catArray = [];
-
-function makeImageObj(name, path) {
-  this.name = name;
-  this.path = path;
-  this.nClicks = 0;
-}
 
 catArray[0] = new makeImageObj("bag", "img/catInBag.jpg");
 catArray[1] = new makeImageObj("beach", "img/catOnBeach.jpg");
 catArray[2] = new makeImageObj("train", "img/catOnTrain.jpg");
 
+function makeImageObj(name, path) {
+  this.name = name;
+  this.path = path;
+  this.nShow = 0;
+  this.nClicks = 0;
+}
+
 function randomImageIndex() {
-  var result = Math.floor(Math.random() * (catArray.length+1));
+  var result = Math.floor(Math.random() * (catArray.length));
   return result;
 }
