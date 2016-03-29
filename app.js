6e9dea6845a623a16ab247640714a52379de4200
hannah gaskins <hnag409@gmail.com>
2016-03-29 14:22:00 -0700

    so all yall have this shit

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ var catDiv = document.getElementById('catsOnCats');
 
 var newImage = document.getElementById('catImage');
 
-catDiv.addEventListener("click", function() {
-  console.log("this is a click");
-  var clickCount = newImage.getAttribute("src");
-  console.log(clickCount);
-  
-  //replacing image
-  var n = randomImageIndex();
-  newImage.setAttribute("src", catArray[n].path);
-  catArray[n].nShow++;
-})
+var displayButton = document.getElementById('myButton');
 
 var catArray = [];
+
+var totalClicks = 0;
+
+catDiv.addEventListener("click", imageClicked);
 
 catArray[0] = new makeImageObj("bag", "img/catInBag.jpg");
 catArray[1] = new makeImageObj("beach", "img/catOnBeach.jpg");
 catArray[2] = new makeImageObj("train", "img/catOnTrain.jpg");
+
+showRandomImg();
 
 function makeImageObj(name, path) {
   this.name = name;
@@ -26,7 +23,32 @@ function makeImageObj(name, path) {
   this.nClicks = 0;
 }
 
+/*
+random number generation to go through amount of images within
+constructors
+*/
 function randomImageIndex() {
   var result = Math.floor(Math.random() * (catArray.length));
   return result;
+}
+
+//function to display random image from list
+function showRandomImg() {
+  var clickCount = newImage.getAttribute("src");
+  console.log(clickCount);
+
+
+  //replacing image function
+  var n = randomImageIndex();
+  newImage.setAttribute("src", catArray[n].path);
+  catArray[n].nShow++;
+}
+
+function imageClicked() {
+  totalClicks++;
+  showRandomImg();
+  if (totalClicks >= 4) {
+    //code to display hidden button
+    displayButton.setAttribute('style','visibility:visible');
+  }
 }
