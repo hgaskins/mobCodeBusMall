d6a7d1210ad5497cd88a10fe11a3ade4ce6b48ae
hannah gaskins <hnag409@gmail.com>
2016-03-29 18:03:29 -0700

    added in comments and changed counter to 16 clicks

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ variables
 
 // var catDiv = document.getElementById('catsOnCats');
 
+/* ++++++
+specific images set to variables and collect the id from DOM
+*/
 var imageOne = document.getElementById('catImageOne');
 var imageTwo = document.getElementById('catImageTwo');
 var imageThree = document.getElementById('catImageThree');
@@ -25,6 +28,10 @@ variables capturing the paragraph slots
 for textContent addition on show results
 */
 
+/* ++++++
+variables for paragraph elements to be added per
+image and add paragraph elements within html
+*/
 var paraOne = document.getElementById('paraOne');
 var paraTwo = document.getElementById('paraTwo');
 var paraThree = document.getElementById('paraThree');
@@ -35,6 +42,9 @@ arrays
 ===============
 */
 
+/* +++++++
+make a new object here per image added
+*/
 catArray[0] = new makeImageObj("bag", "img/catInBag.jpg");
 catArray[1] = new makeImageObj("beach", "img/catOnBeach.jpg");
 catArray[2] = new makeImageObj("train", "img/catOnTrain.jpg");
@@ -45,6 +55,10 @@ calling functions
 ================
 */
 
+/* ++++++++
+calling the showRandomImg function with specific images here. Will need
+to ammend additional images
+*/
 showRandomImg(imageOne);
 showRandomImg(imageTwo);
 showRandomImg(imageThree);
@@ -56,7 +70,9 @@ functions
 =========
 */
 
-//function to count the number of clicks on imageOne specifically
+/* +++++++++
+function to count the number of clicks on imageOne specifically
+*/
 imageOne.onclick = function() {
   var srcValue = imageOne.getAttribute('src');
 
@@ -92,15 +108,17 @@ imageThree.onclick = function() {
   }
 }
 
+//
 function imageClicked() {
   if (processClick) {
     totalClicks++;
 
+    //for each addition image add in code to call the image here:
     showRandomImg(imageOne);
     showRandomImg(imageTwo);
     showRandomImg(imageThree);
 
-    if (totalClicks >= 5) {
+    if (totalClicks >= 16) {
       //code to display hidden button
       displayButton.setAttribute('style','visibility:visible');
       voteMoreButton.setAttribute('style','visibility:visible');
@@ -109,6 +127,7 @@ function imageClicked() {
   }
 }
 
+//constructor function to make new image objects
 function makeImageObj(name, path) {
   this.name = name;
   this.path = path;
@@ -138,11 +157,14 @@ function showRandomImg(image) {
 }
 
 
-//FIX
+
 //function to show results
 function showResults() {
   console.log(totalClicks + " this is working yes?");
   chart.textContent = "the total number of clicks is " + totalClicks + " and this shows how often the Abercrombie cat showed up: " + catArray[0].nShow;
+  /* ++++++++++
+  add in paraNum.textContent per paragraph element position within HTML DOM
+  */
   paraOne.textContent = "the number of times cat in bag was clicked on " + catArray[0].nClicks;
   paraTwo.textContent = "the number of times catOnBeach has been clicked is " + catArray[1].nClicks;
   paraThree.textContent = "the number of times cat on train has been clicked is " + catArray[2].nClicks;
@@ -156,6 +178,9 @@ event listeners
 ===============
 */
 
+/*++++++
+addin eventListener per imageNUMBER variables set at the top of the js file
+*/
 imageOne.addEventListener("click", imageClicked);
 imageTwo.addEventListener("click", imageClicked);
 imageThree.addEventListener("click", imageClicked);
